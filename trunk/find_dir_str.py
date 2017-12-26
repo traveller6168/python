21df58532be36b30259da53df9e8292af2bb664d
traveller6168 <543675628@qq.com>
2017-12-26 23:35:20 +0800

    闭包

--- trunk/find_dir_str.py
+++ trunk/find_dir_str.py
@@ -1,38 +1,41 @@
 #!/usr/bin/python3
-# encoding=gbk
+# encoding=utf-8
 import os
 
 
 def dirlist(path,var_find_str, allfile, file_result):
+    #print("Search strint is:", var_find_str)
     filelist = os.listdir(path)
     for filename in filelist:
         filepath = os.path.join(path, filename)
-        # print(filepath)
+        #print(filepath)
         if os.path.isdir(filepath):
             dirlist(filepath,var_find_str, allfile, file_result)
         else:
             allfile.append(filepath)
-            # print(filepath,filename)
-            # if '_02047_' in filename[0:2].upper():
-            file_desc = open(filepath, 'r')
+            print("Searching File:",filepath, end = '')
+            #if 'XML' in filename.upper():
+            file_desc = open(filepath, 'rb')
             file_desc_c = file_desc.read()
-            if var_find_str.upper() in file_desc_c.upper():
-                # print (filename,'文件中包含GBASEBAS库\n')
+            #print(str(file_desc_c))
+            if var_find_str.upper() in str(file_desc_c).upper():
                 file_result.append(filename)
+                print("  \033[4;31m exist string: %s\033[0m " %  var_find_str)
             else:
+                print("  \033[0;36m isn't exist string: %s\033[0m "%  var_find_str)
                 pass
             file_desc.close()
-    # print(result_list)
     return allfile, file_result
 
 
 
 while True:
-    find_str = input("请输入要查找的字段：\n")
-    # print (dirlist("/gpfs/yijing/download/", [],[]))
-    if find_str == 'quit@':
+    find_str = input("Please input the string you search:\n")
+    if find_str == 'quit':
         break
     else:
-        return_content = dirlist("F:\资料\pydata-book-master\ch02",find_str, [], [])
+        return_content = dirlist("/home/admin/PycharmProjects/python",find_str, [], [])
         return_file = list(set(return_content[1]))
-        print("结果文件\n", return_file)
+        print("The result is :\n")
+        for var in return_file:
+            print(var)
