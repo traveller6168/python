1788ba5a24614028385ba67974230b07065bb78d
traveller6168 <34788770+traveller6168@users.noreply.github.com>
2017-12-26 23:48:28 +0800

    递归查找某目录及子目录文件内容中包含指定字符串
    递归查找某目录及子目录文件内容中包含指定字符串

--- trunk/find_dir_str.py
+++ trunk/find_dir_str.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # encoding=utf-8
 import os
-
+#递归查找某目录及子目录文件内容中包含指定字符串，递归方法参考网上现有方法，另外python有相关模块可实现同样功能
 
 def dirlist(path,var_find_str, allfile, file_result):
     #print("Search strint is:", var_find_str)
@@ -15,12 +15,12 @@ def dirlist(path,var_find_str, allfile, file_result):
             allfile.append(filepath)
             print("Searching File:",filepath, end = '')
             #if 'XML' in filename.upper():
-            file_desc = open(filepath, 'rb')
+            file_desc = open(filepath, 'rb') #以二进制模式打开文件
             file_desc_c = file_desc.read()
             #print(str(file_desc_c))
-            if var_find_str.upper() in str(file_desc_c).upper():
+            if var_find_str.upper() in str(file_desc_c).upper(): #将内容转换为字符串并转为大写
                 file_result.append(filename)
-                print("  \033[4;31m exist string: %s\033[0m " %  var_find_str)
+                print("  \033[4;31m exist string: %s\033[0m " %  var_find_str) #设置显示文字前景色为红色，背景色为默认
             else:
                 print("  \033[0;36m isn't exist string: %s\033[0m "%  var_find_str)
                 pass
